706d4dde866fedf539dcc75df06294223d9e320d
charsh01 <johndoe@example.com>
2023-10-21 11:42:04 -0400

    working commit.

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,18 @@
--- Add your sql here
+
+WITH unpaid AS (
+SELECT *
+FROM customers
+JOIN orders
+ON customers.customer_id = orders.customer_id
+WHERE order_status = 'unpaid'),
+
+balance AS (
+SELECT *
+FROM unpaid
+JOIN subscriptions
+ON subscriptions.subscription_id = unpaid.subscription_id)
+
+SELECT customer_name AS "Customer", printf('$%.2f', price_per_month*subscription_length) AS "Amount Due"
+FROM balance;
+
+
